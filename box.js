9247fe818c9a772fd1b7e9d2697fb7278fd93b4c
DONJEFFREY720 <95631932+DONJEFFREY720@users.noreply.github.com>
2022-01-24 15:03:26 +0530

    Update box.js

--- box.js
+++ box.js
@@ -4,13 +4,12 @@ class Box
     {
       this.x =100;
       this.y = 200
-      this.w = 50;
-      this.h = 50;
+      this.r = 50;
     }
     
     show()
     {
-      rect(this.x,this.y,this.w,this.h)
+      circle(this.x,this.y,this.r)
     }
     
     //write function to set the width of the box
